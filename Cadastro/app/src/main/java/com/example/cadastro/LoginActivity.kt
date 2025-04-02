0af67f3d3095e4258f3ef19aa460c625ccfcc0e0
Alvaro-Heitor <alvaro.heitor0@gmail.com>
2025-04-02 19:34:07 -0300

    Adicionando app duas telas

---
+++ Cadastro/app/src/main/java/com/example/cadastro/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.cadastro
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.imageView2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<FloatingActionButton>(R.id.botãoVoltar).setOnClickListener {
+            finish()
+        }
+    }
+}
